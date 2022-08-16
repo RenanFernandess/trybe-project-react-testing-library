d996d0949a48fadc6a8bbe4c95d28ff1d0375b3e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-16 19:46:44 -0300

    REQUISITO-3: Testa se são exibidos todos os cards de pokémons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
 
-describe('Teste o componente <FavoritePokemons.js />', () => {
+describe('Testa o componente <FavoritePokemons.js />', () => {
   it(`Teste se é exibida na tela a mensagem No favorite pokemon found,
   caso a pessoa não tenha pokémons favoritos`,
   () => {
@@ -16,5 +17,22 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
 
     expect(message).toBeInTheDocument();
     expect(message).toHaveTextContent(textMesMessage);
+  });
+
+  it('Testa se são exibidos todos os cards de pokémons favoritados', () => {
+    const INDEX = 3;
+    const Favorites = pokemons.slice(0, INDEX);
+
+    renderWithRouter(<FavoritePokemons pokemons={ Favorites } />);
+
+    const favoritesCard = screen.getAllByTestId('pokemon-name');
+
+    const [pikachu, charmander, caterpie] = favoritesCard;
+    const favoritesNumber = 3;
+
+    expect(favoritesCard).toHaveLength(favoritesNumber);
+    expect(pikachu).toHaveTextContent('Pikachu');
+    expect(charmander).toHaveTextContent('Charmander');
+    expect(caterpie).toHaveTextContent('Caterpie');
   });
 });
