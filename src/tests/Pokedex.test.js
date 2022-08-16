c667bb4c4ae181f4a3a4d471f9bdd626d120d447
Renan Fernandes <renzinestuods@gmail.com>
2022-08-16 20:10:20 -0300

    REQUISITO-5: Testa se a página contém um heading h2 com o texto Encountered pokémons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+
+    const titleText = /^Encountered pokémons$/i;
+    const title = screen.getByRole('heading', { level: 2, name: titleText });
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(titleText);
+  });
+});
