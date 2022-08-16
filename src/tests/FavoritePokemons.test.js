09a03b1497d8ba5d3269122b8f929b0eb5be81f3
Renan Fernandes <renzinestuods@gmail.com>
2022-08-16 19:25:45 -0300

    REQUISITO-3: Testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it(`Teste se é exibida na tela a mensagem No favorite pokemon found,
+  caso a pessoa não tenha pokémons favoritos`,
+  () => {
+    const noFavorites = [];
+
+    renderWithRouter(<FavoritePokemons pokemons={ noFavorites } />);
+
+    const textMesMessage = /^No favorite pokemon found$/i;
+    const message = screen.getByText(textMesMessage, { selector: 'p' });
+
+    expect(message).toBeInTheDocument();
+    expect(message).toHaveTextContent(textMesMessage);
+  });
+});
