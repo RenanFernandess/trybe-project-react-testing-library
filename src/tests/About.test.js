fd294781a23dd4dd977c168421e2242b6ffe572e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-16 17:35:35 -0300

    REQUISITO-2: Testa se a página contém as informações sobre a Pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <About.js />.', () => {
+  it('se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphOneText = /^This application simulates/i;
+    const paragraphTwoText = /^One can filter Pokémons by type, and see more details/i;
+    const paragraphOne = screen.getByText(paragraphOneText);
+    const paragraphTwo = screen.getByText(paragraphTwoText);
+
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragraphOne).toHaveTextContent(paragraphOneText);
+    expect(paragraphTwo).toBeInTheDocument();
+    expect(paragraphTwo).toHaveTextContent(paragraphTwoText);
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const titleText = /^About Pokédex/i;
+    const title = screen.getByRole('heading', { level: 2, name: titleText });
+
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(titleText);
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphsText = /Pokémons/i;
+    const paragraphs = screen.getAllByText(paragraphsText);
+    const numberOfParagraphs = 2;
+
+    expect(paragraphs).toHaveLength(numberOfParagraphs);
+  });
+});
