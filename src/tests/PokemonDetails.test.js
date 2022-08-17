e1eed768fddc1acfa5f88e314908211664d7d9d6
Renan Fernandes <renzinestuods@gmail.com>
2022-08-17 18:56:38 -0300

    REQUISITO-7: Teste se existe na página uma seção com os mapas contendo as localizações do pokémon

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,10 +4,14 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-const pokemonDetailsTitleText = /Pikachu Details/i;
+const pokemonDetailsTitleText = /^Pikachu Details$/i;
 const titleSummaryText = /^summary$/i;
 const pokemonSummaryText = /^This intelligent Pokémon roasts hard berries with/i;
 const linkText = /^More details$/i;
+const gameLocationsTitleText = /^Game Locations of Pikachu$/i;
+const pokemonLocationImageAlt = /^Pikachu location$/i;
+const locationImageOneSrc = 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png';
+const locationImageTwoSrc = 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png';
 
 describe('Testa o componente <PokemonDetails.js />', () => {
   it('Testa se as informações detalhadas do pokémon selecionado são mostradas na tela',
@@ -28,4 +32,27 @@ describe('Testa o componente <PokemonDetails.js />', () => {
       expect(summary).toBeInTheDocument();
       expect(pokemonSummary).toBeInTheDocument();
     });
+
+  it(`Teste se existe na página uma seção com os mapas contendo
+    as localizações do pokémon`,
+  () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+
+    const gamesLocationsTitle = screen.getByRole(
+      'heading',
+      { level: 2, name: gameLocationsTitleText },
+    );
+    const locationImage = screen.getAllByRole('img', { name: pokemonLocationImageAlt });
+    const [locationImageOne, locationImageTwo] = locationImage;
+    const locationNameOne = screen.getByText('Kanto Viridian Forest', { selector: 'em' });
+    const locationNameTwo = screen.getByText('Kanto Power Plant', { selector: 'em' });
+
+    expect(locationImage).toHaveLength(2);
+    expect(locationNameOne).toBeInTheDocument();
+    expect(locationNameTwo).toBeInTheDocument();
+    expect(locationImageOne).toHaveAttribute('src', locationImageOneSrc);
+    expect(locationImageTwo).toHaveAttribute('src', locationImageTwoSrc);
+    expect(gamesLocationsTitle).toBeInTheDocument();
+  });
 });
